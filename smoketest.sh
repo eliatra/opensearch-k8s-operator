57602e37567bdacd4f0e6837655b44ef2eec2097
Eliatra <git@eliatra.com>
2023-11-26 18:18:45 +0100

    Make smoketest wait for ready cluster

--- smoketest.sh
+++ smoketest.sh
@@ -10,5 +10,26 @@ export KUBECONFIG=$(pwd)/kubeconfig-smoketest
 helm install eoko oci://registry-1.docker.io/eliatra/eoko --wait
 helm install eoko-cluster oci://registry-1.docker.io/eliatra/eoko-cluster --wait
 
+echo
+echo "==============================================================="
+echo
+echo "Waiting until OpenSearch is ready ..."
+echo
+until kubectl wait -l statefulset.kubernetes.io/pod-name=eoko-cluster-masters-2 --for=condition=ready pod --timeout=-1s
+do
+    echo "Waiting for eoko-cluster-masters-2 pod to be ready ..."
+    sleep 30
+done
+
+until kubectl rollout status deployment eoko-cluster-dashboards
+do
+    echo "Waiting for eoko-cluster-dashboards deployment to be ready ..."
+    sleep 15
+done
+
+echo
+echo "==============================================================="
+echo "Finished successfully"
+
 ## Delete k3d cluster
 k3d cluster delete $CLUSTER_NAME
